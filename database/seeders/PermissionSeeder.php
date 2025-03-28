c4c87e5fb049f66e66a2675160845c1fb637d703
Mohit  Rathore <mohit.foxaisr@gmail.com>
2025-03-28 17:51:46 +0530

    Initial Commit

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Permission::firstOrCreate(['name' => 'WeeklyReports.manage']);
+        // Permission::firstOrCreate(['name' => 'WeeklyReports.download']);
+    }
+}
