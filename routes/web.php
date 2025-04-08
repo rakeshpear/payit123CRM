0c9202fcb6e402f96324b45c17a5af2ac7b58c3b
Mohit  Rathore <mohit.foxaisr@gmail.com>
2025-04-08 11:28:02 +0530

    Changes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Log;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
